446567669bb48834f87d601ee8fc6c02b034eca3
NPNKhoa <khoa.nphucnguyen@gmail.com>
2024-09-04 15:05:32 +0700

    update: add delete all to cart end point

--- src/routes/cart.route.js
+++ src/routes/cart.route.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {
   addToCart,
   changeItemQuantity,
+  deleteAllFromCart,
   deleteFromCart,
   getCartByUser,
 } from '../controllers/cart.controller.js';
@@ -14,6 +15,8 @@ router.get('/', auth, getCartByUser);
 router.post('/add', auth, addToCart);
 
 router.put('/update-quantity', auth, changeItemQuantity);
+
+router.delete('/all', auth, deleteAllFromCart);
 
 router.delete('/:id', auth, deleteFromCart);
 
