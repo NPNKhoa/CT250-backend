3d734126c3bd414d14fa95197cdd1fb0d4832d97
tu1511 <131641671+tu1511@users.noreply.github.com>
2024-11-03 23:37:28 +0700

    feat: get data all comments

--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import {
   createComment,
+  getAllComments,
   getAllProductComment,
 } from '../controllers/comment.controller.js';
 import { auth } from '../middlewares/authentication.js';
 
 const router = express.Router();
 
-router.get('/by-product', getAllProductComment);
+router.get('/byproduct', getAllProductComment);
+router.get('/all', getAllComments);
 
 router.post('/', auth, createComment);
 
