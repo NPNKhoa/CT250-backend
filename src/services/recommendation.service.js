79c6025a1f9a55a3cfdbb0bc19e30218bafcbcbd
NPNKhoa <khoa.nphucnguyen@gmail.com>
2024-11-10 00:06:59 +0700

    fix: fix suggest products api

--- src/services/recommendation.service.js
+++ src/services/recommendation.service.js
@@ -4,7 +4,7 @@ import { User } from '../models/user.model.js';
 import { Product } from '../models/product.model.js';
 
 export class RecommendationService {
-  static async suggestProduct(userId, recentViewedProducts) {
+  static async suggestProduct(userId) {
     const purchasedProducts = (
       await Order.find({ user: userId })
         .populate({
@@ -18,7 +18,7 @@ export class RecommendationService {
         })
         .select('orderDetail')
     ).flatMap((order) =>
-      order.orderDetail.map((cartDetail) => cartDetail.product._id.toString())
+      order.orderDetail.map((cartDetail) => cartDetail.product._id)
     );
 
     const reviewedProducts = (
@@ -29,10 +29,9 @@ export class RecommendationService {
         model: 'Product',
         select: '_id',
       })
-    ).map((review) => review.product._id.toString());
+    ).map((review) => review.product._id);
 
     const interactedProducts = new Set([
-      ...recentViewedProducts,
       ...purchasedProducts,
       ...reviewedProducts,
     ]);
@@ -116,7 +115,6 @@ export class RecommendationService {
     console.log(similarUsers);
 
     const recommendedProducts = await Product.find({
-      //   _id: { $nin: Array.from([...purchasedProducts, ...reviewedProducts]) },
       _id: {
         $in: similarUsers.map((product) => product._id),
       },
